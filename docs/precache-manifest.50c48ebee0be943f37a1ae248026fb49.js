6c4a41b3dca6d661365cb50f64ba808803bbeeaf
Max <max.andres.rf@gmail.com>
2021-03-26 00:24:36 -0600

    commit docs

---
+++ docs/precache-manifest.50c48ebee0be943f37a1ae248026fb49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1c8d64f0350c3d4b90484b91816d7c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c257c1874f6bf708adf",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c257c1874f6bf708adf",
+    "url": "/static/js/main.d8288d37.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
